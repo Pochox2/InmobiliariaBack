45a4ed65c0333d4e8b31cbab1d4cf430b2bfe215
Leandro Insua <leooinsua79@gmail.com>
2026-08-01 20:56:48 -0300

    pasos finales

---
+++ Inmo.Infra/Data/DbSeed.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inmo.Infra.Data
+{
+    internal class DbSeed
+    {
+    }
+}
